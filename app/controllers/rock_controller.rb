50aba59980902481cea9dd218bf146992442b2bb
mollymoog <mmkover@gmail.com>
2020-10-11 15:43:33 -0400

    remove bindings

--- app/controllers/rock_controller.rb
+++ app/controllers/rock_controller.rb
@@ -4,11 +4,13 @@ class RockController < ApplicationController
 
 
       get "/rocks/new" do
+        if !logged_in?
+          redirect "/geologists/welcome"
+        end
         erb :'rocks/new'
       end
 
       post "/rocks" do
-        binding.pry
         @rock = current_geologist.rocks.build(params["rock"])
         if @rock.save
           redirect '/geologists/home'
@@ -16,17 +18,22 @@ class RockController < ApplicationController
       end
 
       get "/rocks/:id" do
+        if !logged_in?
+          redirect "/geologists/welcome"
+        end
         @rock = Rock.find_by_id(params[:id])
         erb :'rocks/show'
       end
 
       get "/rocks/:id/edit" do
+        if !logged_in?
+          redirect "/geologists/welcome"
+        end
         @rock = Rock.find_by_id(params[:id])
         erb :'rocks/edit'
       end
 
       patch "/rocks/:id" do
-        binding.pry
         @rock = Rock.find_by_id(params[:id])
         @rock.update(params["rock"])
         redirect "rocks/#{@rock.id}"
